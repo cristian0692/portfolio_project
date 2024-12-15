f37b80cb6335adfd6907861feb0c6a5054227546
cristian <cr.bolohan@gmail.com>
2024-12-15 08:04:42 +0100

    imported_assets

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -47,7 +47,7 @@ const Hero = () => {
           </h2>
           <TextGenerateEffect
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
-            words="Transforming Concepts into Seamless Experiences"
+            words="Transforming Concepts into Seamless User Experiences"
           />
           <p
             className="text-center md:tracking-wider 
